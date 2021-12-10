18e6789a49b55d92459f721ba7e7922b99250daf
samruddhideode <samruddhi.deode@cummminscollege.in>
2021-12-10 13:13:51 +0530

    visualization candlestick graphs added
    to be done: X axis dates of candlestick graph

--- app/src/main/java/com/example/cryptotracker/Visualization.java
+++ app/src/main/java/com/example/cryptotracker/Visualization.java
@@ -1,10 +1,16 @@
 package com.example.cryptotracker;
 
+import java.util.*;
+import java.text.*;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.*;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import okhttp3.Call;
@@ -17,10 +23,18 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.cryptotracker.Model.CurrencyModal;
+import com.github.mikephil.charting.charts.CandleStickChart;
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.CandleData;
+import com.github.mikephil.charting.data.CandleDataSet;
+import com.github.mikephil.charting.data.CandleEntry;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ICandleDataSet;
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 import com.google.gson.Gson;
 
@@ -33,8 +47,12 @@ import java.lang.reflect.Type;
 import java.util.ArrayList;
 
 public class Visualization extends AppCompatActivity {
-    private LineChart lineChart;
-    private Button homeExpButton;
+    private CandleStickChart candleStickChart;
+    private Button apply;
+    private RadioGroup coins;
+    private RadioGroup time;
+    private RadioButton coin_selected;
+    private RadioButton time_selected;
     Request request;
     OkHttpClient client;
 
@@ -43,27 +61,33 @@ public class Visualization extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.visualization);
 
-        lineChart = findViewById(R.id.lineChart);
-        homeExpButton = (Button) findViewById(R.id.homeButton);
+        candleStickChart = findViewById(R.id.candleStick);
+        coins = findViewById(R.id.coins);
+        time = findViewById(R.id.time);
+        apply = findViewById(R.id.apply);
 
-        showLineChart();
-
-        homeExpButton.setOnClickListener(new View.OnClickListener(){
+        apply.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent explicit_home = new Intent(Visualization.this, MainActivity.class);
-                startActivity(explicit_home);
+            public void onClick(View v) {
+                int radio_coin_id = coins.getCheckedRadioButtonId();
+                coin_selected = findViewById(radio_coin_id);
+                int radio_time_id = time.getCheckedRadioButtonId();
+                time_selected = findViewById(radio_time_id);
+
+                String coin = coin_selected.getText().toString();
+                String time = time_selected.getText().toString();
+                System.out.println("COIN TIME "+coin+time);
+                showCandleStickChart(coin,time);
             }
         });
 
     }
-
-    private void showLineChart(){
-        ArrayList<Entry> toReturn = new ArrayList<>();
+    private void showCandleStickChart(String coin, String time){
         final ArrayList<ArrayList<Long>> dataSet = new ArrayList<>();
         client = new OkHttpClient();
+        String url = "https://api.gemini.com/v2/candles/"+ coin + "/"+ time;
         request = new Request.Builder().url(
-                String.format("https://api.gemini.com/v2/candles/btcusd/1day")).build();
+                String.format(url)).build();
 
         client.newCall(request).enqueue(new Callback() {
             @Override
@@ -72,27 +96,70 @@ public class Visualization extends AppCompatActivity {
                 try {
                     //getting json array of array response in dataSet [time, open, high, low, close, volume]
                     JSONArray itemArray = new JSONArray(jsonString);
-                    //System.out.println("item array: "+itemArray);
                     for (int i = 0; i < itemArray.length(); i++) {
-                        //System.out.println("I: "+itemArray.getJSONArray(i));
                         ArrayList<Long> eachCoin = new ArrayList<>();
                         for(int j=0; j<itemArray.getJSONArray(i).length(); j++){
                             eachCoin.add(itemArray.getJSONArray(i).getLong(j));
                         }
                         dataSet.add(eachCoin);
                     }
-                    for (int arr =0; arr<dataSet.size(); arr++){
-                        toReturn.add(new Entry(arr,dataSet.get(arr).get(2)));
-                    }
-                    //display graph
-                    LineDataSet lineDataSet = new LineDataSet(toReturn, "data set");
-                    System.out.println("HERE" + lineDataSet);
-                    ArrayList<ILineDataSet> iLineDataSets = new ArrayList<>();
-                    iLineDataSets.add(lineDataSet);
 
-                    LineData lineData = new LineData(iLineDataSets);
-                    lineChart.setData(lineData);
-                    lineChart.invalidate();
+                    //styling chart layout
+                    YAxis yAxis = candleStickChart.getAxisLeft();
+                    YAxis rightAxis = candleStickChart.getAxisRight();
+                    yAxis.setDrawGridLines(false);
+                    rightAxis.setDrawGridLines(false);
+                    candleStickChart.requestDisallowInterceptTouchEvent(true);
+
+                    XAxis xAxis = candleStickChart.getXAxis();
+
+                    xAxis.setDrawGridLines(false);// disable x axis grid lines
+                    xAxis.setDrawLabels(false);
+                    rightAxis.setTextColor(Color.BLACK);
+                    yAxis.setDrawLabels(false);
+                    xAxis.setGranularity(1f);
+                    xAxis.setGranularityEnabled(true);
+                    xAxis.setAvoidFirstLastClipping(true);
+
+                    Legend l = candleStickChart.getLegend();
+                    l.setEnabled(true);
+                    candleStickChart.setHighlightPerDragEnabled(true);
+                    candleStickChart.setDrawBorders(true);
+                    candleStickChart.setBorderColor(getResources().getColor(R.color.purple_700));
+
+                    //setting data
+                    ArrayList<String> xvalue = new ArrayList<String>();
+                    ArrayList<CandleEntry> yvalue = new ArrayList<CandleEntry>();
+                    for (int i=0; i<dataSet.size(); i++) {
+                        //code to convert unix timestamp to day date time
+                        long unix_seconds = dataSet.get(i).get(0);
+                        Date date = new Date(unix_seconds);
+                        String[] splitted = date.toString().split("\\s+");//split by space  Wed Dec 09 09:30:00 GMT+05:30 2020
+
+                        //dates on x axis
+                        xvalue.add(splitted[2] + splitted[1] + splitted[5]);
+                        //data on y axis
+                        yvalue.add(new CandleEntry(i, dataSet.get(i).get(2), dataSet.get(i).get(3), dataSet.get(i).get(1), dataSet.get(i).get(4)));
+                    }
+
+                    //styling data layout
+                        CandleDataSet set1 = new CandleDataSet(yvalue,"Dataset1");
+                        set1.setColor(Color.rgb(80, 80, 80));
+                        set1.setShadowColor(getResources().getColor(R.color.purple_200));
+                        set1.setShadowWidth(0.8f);
+                        set1.setDecreasingColor(getResources().getColor(R.color.purple_500));
+                        set1.setDecreasingPaintStyle(Paint.Style.FILL);
+                        set1.setIncreasingColor(getResources().getColor(R.color.purple_700));
+                        set1.setIncreasingPaintStyle(Paint.Style.FILL);
+                        set1.setNeutralColor(Color.LTGRAY);
+                        set1.setDrawValues(false);
+
+                        //creating a data object with the dataset
+                        CandleData data = new CandleData(set1);
+
+                        //set data
+                        candleStickChart.setData(data);
+                        candleStickChart.invalidate();
 
                 } catch (JSONException e) {
                     e.printStackTrace();
